50291b6ff033616eb2a3cbb11d2cefc1412b99db
Wareez12345 <akinkunmiwareez@gmail.com>
2022-07-06 23:38:24 -0700

    Task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,17 @@
 #include "main.h"
 /**
-* print_alphabet - entry point
-* Return: 0 Success
+* main - check the code
+* 
+* Return: Always 0.
 */
-void print_alphabet(void)
+int main(void)
 {
+print_alphabet();
 char j;
 for (j = 'a'; j <= 'z'; j++)
 {
 _putchar(j);
 }
 _putchar('\n');
+return (0);
 }
