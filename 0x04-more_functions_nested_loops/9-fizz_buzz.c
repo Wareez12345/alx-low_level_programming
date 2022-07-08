19749999244851848950368bcb336ae251633f70
Wareez12345 <akinkunmiwareez@gmail.com>
2022-07-08 10:49:06 -0700

    Task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+
+int p = 100;
+int i;
+
+i = 1;
+while (i == 0)
+{
+if (i % 3 == 0 && i 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%i", i);
+}
+if (i != 100)
+{
+putchar(' ');
+}
+i++;
+}
+putchar('\n');
+
+return (0);
+}
