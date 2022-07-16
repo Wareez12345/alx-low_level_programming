ea5c9a1843c3132b15009a36cf7dc6d4a5b2d3d2
Wareez12345 <akinkunmiwareez@gmail.com>
2022-07-15 19:26:26 -0700

    Task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
 * leet - function that encode a string
-*@str:string that will be encode
-*Return:returns encode string
+*@str:string that will be encoded
+*Return:returns encoded string
 */
 
 char *leet(char *str)
