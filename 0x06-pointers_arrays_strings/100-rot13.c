446cbcf7664fa26052cffd5083d2e45b8947f8b3
Wareez12345 <akinkunmiwareez@gmail.com>
2022-07-15 20:18:08 -0700

    Task

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -19,6 +19,7 @@ char *rot13(char *str)
 		'k', 'l', 'm', 'n', 'o', 'p',
 		'q', 'r', 's', 't', 'u', 'v',
 		'w', 'x', 'y', 'z'};
+
 	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 		'T', 'U', 'V', 'W', 'X', 'Y'
 		'Z', 'A', 'B', 'C', 'D', 'E',
@@ -28,6 +29,7 @@ char *rot13(char *str)
 		'x', 'y', 'z', 'a', 'b', 'c',
 		'd', 'e', 'f', 'g', 'h', 'i',
 		'j', 'k', 'l', 'm'};
+
 	while (str[++index1])
 	{
 		for (index2 = 0; index2 < 52; index2++)
